e401ba89ce44cae4450f05b7c02afc9d368965d4
Vishwajeet mishra <mishravishwajeet312@gmail.com>
2026-04-04 15:41:45 +0530

    Add JsonIgnoreProperties and JsonIgnore annotations

--- Backend/src/main/java/com/example/studenttable/entity/Student.java
+++ Backend/src/main/java/com/example/studenttable/entity/Student.java
@@ -1,6 +1,7 @@
 package com.example.studenttable.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,6 +15,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Student {
 
     @Id
@@ -33,6 +35,7 @@ public class Student {
     @Column(nullable = false)
     private String role;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<StudentCourse> enrolledCourses = new ArrayList<>();
 }
